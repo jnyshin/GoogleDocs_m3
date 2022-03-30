978329c51f2c3cae86987c52302ba558fffeff53
seoeongeueun <erin9901@gmail.com>
2022-03-30 00:53:04 -0400

    add css to header
    logo icon and header option buttons

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -5,14 +5,31 @@ import './App.css';
 
 function App() {
   const [body, setBody ] = useState("");
+  const [title, setTitle ] = useState("");
 
   const handleBody = e => {
     setBody(e);
   }
 
+  function onTitleChange(e){
+    setTitle(e.target.value)
+  }
+
   return (
     <div className="App">
-      <h2>Text Editor</h2>
+      <div className="header">
+        <img src={require("./docs.png")} alt="homepage" width="40px"/>
+        <div className="header-options">
+          <input type="text" onChange={onTitleChange} placeHolder="Untitled"/>
+          <div className="header-btns">
+            <a href="/">File</a>
+            <a href="/">Edit</a>
+            <a href="/">View</a>
+            <a href="/">Help</a>
+          </div>
+        </div>
+        
+      </div>
       <ReactQuill
         placeholder="Start writing..."
         modules={App.modules}
