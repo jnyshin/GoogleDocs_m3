28191d4477d8a0f3e4489cefccf06841889675c5
seoeongeueun <erin9901@gmail.com>
2022-03-29 23:47:37 -0400

    added quill text editor
    just quill

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
+import ReactQuill from "react-quill"
+import "../node_modules/react-quill/dist/quill.snow.css"
 import './App.css';
 
 function App() {
+  const [body, setBody ] = useState("");
+
+  const handleBody = e => {
+    setBody(e);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reloadd.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Text Editor</h2>
+      <ReactQuill
+        placeholder="Start writing..."
+        modules={App.modules}
+        formats={App.formats}
+        onChange={handleBody}
+        value={body}
+      />
     </div>
   );
 }
+
+App.modules = {
+  toolbar: [
+    [{ header: "1" }, { header: "2" }, { header: [3, 4, 5, 6]}, { font: [] }],
+    [{ size: [] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    ["link", "image", "video"],
+    ["clean"],
+    ["code-block"],
+  ],
+};
+
+App.formats = [
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "image",
+  "video",
+  "code-block",
+];
 
 export default App;
