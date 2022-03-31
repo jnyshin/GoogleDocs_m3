32b7f0981f833f8261786037870c0ff75b7cf3df
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2022-03-30 22:42:53 -0400

    Update Socket IO environment

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,26 +1,53 @@
-import React, { useState } from "react"
-import ReactQuill from "react-quill"
-import "../node_modules/react-quill/dist/quill.snow.css"
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import { useParams } from "react-router-dom";
+import { io } from "socket.io-client";
+import API from "./api";
 function App() {
-  const [body, setBody ] = useState("");
-  const [title, setTitle ] = useState("");
+  const [body, setBody] = useState("");
+  const [title, setTitle] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [socket, setSocket] = useState();
 
-  const handleBody = e => {
+  const params = useParams();
+  useEffect(() => {
+    setLoading(true);
+    const socket = io("http://localhost:8000");
+    setSocket(socket);
+
+    const getData = async () => {
+      const response = await API.get(`connect/${params.id}`);
+      const { data } = response;
+      setBody(data);
+      setLoading(false);
+    };
+    getData();
+  }, []);
+
+  const handleBody = (e) => {
     setBody(e);
+  };
+
+  function onTitleChange(e) {
+    setTitle(e.target.value);
   }
 
-  function onTitleChange(e){
-    setTitle(e.target.value)
+  if (loading) {
+    return <div className="loading">Loading</div>;
   }
 
   return (
     <div className="App">
       <div className="header">
-        <img src={require("./docs.png")} alt="homepage" width="40px"/>
+        <img src={require("./docs.png")} alt="homepage" width="40px" />
         <div className="header-options">
-          <input type="text" onChange={onTitleChange} placeHolder="Untitled"/>
+          <input
+            type="text"
+            onChange={onTitleChange}
+            placeholder="Untitled"
+            disabled
+          />
           <div className="header-btns">
             <a href="/">File</a>
             <a href="/">Edit</a>
@@ -28,13 +55,13 @@ function App() {
             <a href="/">Help</a>
           </div>
         </div>
-        
       </div>
       <ReactQuill
         placeholder="Start writing..."
         modules={App.modules}
         formats={App.formats}
         onChange={handleBody}
+        style={{ height: "80vh" }}
         value={body}
       />
     </div>
@@ -43,7 +70,7 @@ function App() {
 
 App.modules = {
   toolbar: [
-    [{ header: "1" }, { header: "2" }, { header: [3, 4, 5, 6]}, { font: [] }],
+    [{ header: "1" }, { header: "2" }, { header: [3, 4, 5, 6] }, { font: [] }],
     [{ size: [] }],
     ["bold", "italic", "underline", "strike", "blockquote"],
     [{ list: "ordered" }, { list: "bullet" }],
--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} exact/>
+        <Route path="/connect/:id" element={<App />}/>
+      </Routes>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
