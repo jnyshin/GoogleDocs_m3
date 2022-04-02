2d0022977e01573b70c8015efb8393fa90c4f563
jnyshin <61779849+jnyshin@users.noreply.github.com>
2022-04-02 01:57:33 -0400

    done routing

--- packages/server/routes/doc.js
+++ packages/server/routes/doc.js
@@ -2,7 +2,7 @@ import express from "express";
 import Conn from "../schema_conn";
 
 const router = express.Router();
-router.get("/", async (req, res) => {
+router.get("/:id", async (req, res) => {
   console.log("doc router reached");
   const id = req.params.id;
   console.log(id);
