9faf2e56072a8eda848a2dd8b3749aeda976067f
jnyshin <61779849+jnyshin@users.noreply.github.com>
2022-04-02 19:43:15 -0400

    weird query

--- packages/server/routes/doc.js
+++ packages/server/routes/doc.js
@@ -1,18 +1,24 @@
 import express from "express";
 import Conn from "../schema_conn";
-
+import { DOCUMENT_ID } from "../store";
+import { QuillDeltaToHtmlConverter } from "quill-delta-to-html";
+import e from "express";
 const router = express.Router();
+
 router.get("/:id", async (req, res) => {
   console.log("doc router reached");
-  const id = req.params.id;
-  console.log(id);
-  await Conn.findOne({ _id: id }, (err, doc) => {
-    if (doc) {
-      console.log(doc);
-      res.send(doc);
+  await Conn.findById(DOCUMENT_ID).exec((doc, err) => {
+    if (!doc) {
+      console.log("not found doc");
+      const ops = err.data.ops;
+      var cfg = {};
+      const converter = new QuillDeltaToHtmlConverter(ops, cfg);
+      const html = converter.convert();
+      console.log(html);
+      res.send(html);
     } else {
-      console.log(err);
-      res.send("ERROR");
+      console.log("found doc", ops);
+      res.send(doc);
     }
   });
 });
