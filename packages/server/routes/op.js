13b71bf61aa48aaadd405e1e7c8544bd3bd215ea
jnyshin <61779849+jnyshin@users.noreply.github.com>
2022-04-02 18:40:34 -0400

    /op done

--- packages/server/routes/op.js
+++ packages/server/routes/op.js
@@ -1,5 +1,4 @@
 import express from "express";
-//import { Connection } from "mongoose";
 import { clients, DOCUMENT_ID } from "../store";
 import Conn from "../schema_conn";
 const router = express.Router();
@@ -7,17 +6,17 @@ const router = express.Router();
 router.post("/:id", async (req, res) => {
   const id = req.params.id;
   const contents = req.body.contents;
-  console.log("server received");
   const delta = req.body.delta;
   const payload = { action: "update", data: delta };
+  res.send("success");
   clients.forEach((client) => {
     if (client.id !== id) {
       client.res.write(`data: ${JSON.stringify(payload)}\n\n`);
     }
   });
-  // await Conn.findByIdAndUpdate(DOCUMENT_ID, { data: contents }).then(
-  //   console.log("Document saved")
-  // );
+  await Conn.findByIdAndUpdate(DOCUMENT_ID, { data: contents }).then(
+    console.log("Document saved")
+  );
 });
 
 export default router;
