eee5fb8ac9fc8fb17d0a56c5978d2c215168d386
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2022-04-13 13:10:30 -0400

    update production

--- packages/client/src/store.js
+++ packages/client/src/store.js
@@ -1,3 +1,3 @@
-// const DOMAIN_NAME = "209.94.58.4";
-const DOMAIN_NAME = "localhost:80";
+const DOMAIN_NAME = "209.151.154.140";
+// const DOMAIN_NAME = "localhost:80";
 export default DOMAIN_NAME;
