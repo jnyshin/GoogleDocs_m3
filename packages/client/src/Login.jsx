0441e3d175e6802a68ea370107de4290e751c4e0
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2022-04-13 01:39:29 -0400

    Update Auth frontend

--- packages/client/src/Login.jsx
+++ packages/client/src/Login.jsx
@@ -3,51 +3,51 @@ import API from "./api";
 import { useHistory } from "react-router-dom";
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const body = {
-        email: email,
-        password: password,
-        };
-        localStorage.setItem('email', email);
-        const user = await API.post("/users/login", body);
-        if (user.data.error) {
-        console.log(user.data.message);
-        }
-        if (user.data.status === "OK") {
-        history.push("/home");
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const body = {
+      email: email,
+      password: password,
+    };
+    localStorage.setItem("email", email);
+    const user = await API.post("/users/login", body);
+    if (user.data.error) {
+      console.log(user.data.message);
     }
+    if (user.data.status === "OK") {
+      history.push("/home");
+    }
+  };
 
-    return (
-        <div className="wrapper">
-            <div className="container">
-                <form method="post" action="/users/login" onSubmit={handleSubmit}>
-                <label htmlFor="email">email:</label>
-                <input
-                    type="text"
-                    id="email"
-                    name="email"
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <label htmlFor="password">Password:</label>
-                <input
-                    type="text"
-                    id="password"
-                    name="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <button type="submit">Login</button>
-                </form>
-                <form method="post" action="/users/logout">
-                <button type="submit">LOGOUT</button>
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <form method="post" action="/users/login" onSubmit={handleSubmit}>
+          <label htmlFor="email">email:</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="text"
+            id="password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Login</button>
+        </form>
+        <form method="post" action="/users/logout">
+          <button type="submit">LOGOUT</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 export default Login;
--- packages/client/src/Signup.jsx
+++ packages/client/src/Signup.jsx
@@ -2,38 +2,53 @@ import React, { useState } from "react";
 import API from "./api";
 
 const Signup = () => {
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
-    const [email, setEmail] = useState("")
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
-    const handleSubmit = async(e) => {
-        e.preventDefault();
-        const body = {
-            username: username,
-            password: password,
-            email: email
-        };
-        const newUser = await API.post("/users/signup", body)
-        if (newUser.data.error) {
-            console.log(newUser.data.message);
-        }
-        console.log(newUser)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const body = {
+      name: name,
+      password: password,
+      email: email,
+    };
+    const newUser = await API.post("/users/signup", body);
+    if (newUser.data.error) {
+      console.log(newUser.data.message);
     }
+    console.log(newUser);
+  };
 
-    return(
-        <div class="wrapper">
-            <div class="container">
-                <form method="post" action="/users/signup" onSubmit={handleSubmit}>
-                    <lable for="username">Username:</lable>
-                    <input type="text" id="username" name="username" onChange={e => setUsername(e.target.value)} />
-                    <lable for="password">Password:</lable>
-                    <input type="text" id="password" name="password" onChange={e => setPassword(e.target.value)}/>
-                    <lable for="email">Email</lable>
-                    <input type="text" id="email" name="email" onChange={e => setEmail(e.target.value)}/>
-                    <input type="submit" value="Submit" />
-                </form>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <form method="post" action="/users/signup" onSubmit={handleSubmit}>
+          <label htmlFor="name">name:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="text"
+            id="password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    </div>
+  );
+};
 export default Signup;
