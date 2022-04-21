99e479fa352587712a51a765e59e5fd7ec258e93
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2022-04-20 21:37:21 -0400

    Update Env

--- packages/client/src/store.js
+++ packages/client/src/store.js
@@ -1,2 +1,5 @@
-const DOMAIN_NAME = "localhost:8000";
+const DOMAIN_NAME =
+  process.env.NODE_ENV === "production"
+    ? "icloud.cse356.compas.cs.stonybrook.edu"
+    : "localhost:8000";
 export default DOMAIN_NAME;
