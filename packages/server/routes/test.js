d7a5f758481139a83c8724661948408aaa00067f
iianjun <iianjun@naver.com>
2022-04-22 12:12:24 -0400

    fix pm2

--- packages/server/routes/test.js
+++ packages/server/routes/test.js
@@ -1,6 +1,39 @@
 export default async (fastify, opts) => {
-  fastify.get("/test", async (req, res) => {
+  fastify.get(`/:key`, async (req, res) => {
+    //return { hello: "world" };
+    //currEditDoc.push("a");
+    //redisClient.get("counter");
+    // redis.get(`counter`, (err, val) => {
+    //   if (err) {
+    //     res.header("X-CSE356", "61f9f57373ba724f297db6ba");
+    //     return err;
+    //   } else {
+    //     console.log(val);
+    //     res.header("X-CSE356", "61f9f57373ba724f297db6ba");
+    //     return `from ${process.pid}, ${val}`;
+    //   }
+    // });
+    const { key } = req.params;
+    console.log(key);
+    console.log(`from ${process.pid}`);
     const { redis } = fastify;
-    console.log(redis);
+    try {
+      const value = await redis.get(key);
+      return { value: value };
+    } catch (err) {
+      return err;
+    }
+  });
+  fastify.post(`/`, async (req, res) => {
+    console.log(`from ${process.pid}`);
+    const { key, value } = req.body;
+
+    const { redis } = fastify;
+    try {
+      await redis.set(key, value);
+      return { status: "ok" };
+    } catch (err) {
+      return err;
+    }
   });
 };
