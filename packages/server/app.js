391eaf6e673d4da56b6a35b90c3673b90ab8fb3d
iianjun <iianjun@naver.com>
2022-04-22 00:19:57 -0400

    update pm2

--- packages/server/app.js
+++ packages/server/app.js
@@ -10,62 +10,63 @@ import fastifyMultipart from "fastify-multipart";
 import fastify from "fastify";
 
 const { NODE_ENV, SECRET } = process.env;
-export default async (app, opts) => {
-  app.register(fastifyCors, {});
+const app = fastify({ logger: true });
 
-  app.register(fastifyCookie, {
-    secret:
-      "hjadksegdjrkhjadksegdjrkhjadksegdjrkhjadksegdjrkhjadksegdjrkhjadksegdjrk",
-    parseOptions: {},
-  });
-  app.register(fastifySession, {
-    secret:
-      "hjadksegdjrkhjadksegdjrkhjadksegdjrkhjadksegdjrkhjadksegdjrkhjadksegdjrk",
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24 * 7,
-      httpOnly: true,
-      secure: false,
-    },
-    saveUninitialized: true,
-    resave: true,
-  });
+app.register(fastifyCors, {});
 
-  app.register(fastifyStatic, {
-    root: join(__dirname, "dist"),
-  });
+app.register(fastifyCookie, {
+  secret:
+    "hjadksegdjrkhjadksegdjrkhjadksegdjrkhjadksegdjrkhjadksegdjrkhjadksegdjrk",
+  parseOptions: {},
+});
+app.register(fastifySession, {
+  secret:
+    "hjadksegdjrkhjadksegdjrkhjadksegdjrkhjadksegdjrkhjadksegdjrkhjadksegdjrk",
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true,
+    secure: false,
+  },
+  saveUninitialized: true,
+  resave: true,
+});
 
-  app.register(fastifyMultipart);
+app.register(fastifyStatic, {
+  root: join(__dirname, "dist"),
+});
 
-  app.register(AutoLoad, {
-    dir: join(__dirname, "routes"),
-  });
-  app.addHook("preHandler", (req, res, next) => {
-    req.log.info(`incoming request from ${req.ip}`);
-    if (!req.url.startsWith("/user")) {
-      if (!req.session.user) {
-        res.header("X-CSE356", "61f9f57373ba724f297db6ba");
-        res.send(ERROR_MESSAGE("Not logged in"));
-      }
+app.register(fastifyMultipart);
+
+app.register(AutoLoad, {
+  dir: join(__dirname, "routes"),
+});
+app.addHook("preHandler", (req, res, next) => {
+  req.log.info(`incoming request from ${req.ip}`);
+  if (!req.url.startsWith("/user")) {
+    if (!req.session.user) {
+      res.header("X-CSE356", "61f9f57373ba724f297db6ba");
+      res.send(ERROR_MESSAGE("Not logged in"));
     }
-    next();
-  });
-  app.get("/", (req, res) => {
-    console.log(`from ${process.pid}`);
-    return { hello: "world" };
-  });
-  app.register((fastifyInstance, options, done) => {
-    mongoose
-      .connect("mongodb://localhost/docs_clone", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then(() => {
-        console.log("mongoose connected");
-      })
-      .catch((err) => {
-        console.error("failed to connect with MongoDB", err);
-        fastifyInstance.close();
-      })
-      .finally(() => done());
-  });
-};
+  }
+  next();
+});
+app.get("/", (req, res) => {
+  console.log(`from ${process.pid}`);
+  return { hello: "world" };
+});
+app.register((fastifyInstance, options, done) => {
+  mongoose
+    .connect("mongodb://localhost/docs_clone", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("mongoose connected");
+    })
+    .catch((err) => {
+      console.error("failed to connect with MongoDB", err);
+      fastifyInstance.close();
+    })
+    .finally(() => done());
+});
+app.listen(NODE_PORT);
--- packages/server/test.js
+++ packages/server/test.js
@@ -1,17 +1,11 @@
-import http from "http";
-const options = ["yes", "no"];
+import fastify from "fastify";
 
-const server = http.createServer((req, res) => {
-  const randomIndex = Math.floor(Math.random() * options.length);
-  const advice = options[randomIndex];
-  const payload = JSON.stringify({
-    processID: process.pid,
-    advice,
-  });
-  console.log(`advice from ${process.pid}: ${advice}`);
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(payload);
+fastify({
+  logger: true,
+});
+fastify.get("/", async (request, reply) => {
+  console.log(`from :${process.pid}`);
+  return { hello: "world" };
 });
 
-server.listen(3000);
-console.log(`advice service running`);
+fastify.listen(3000);
