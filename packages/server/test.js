3b97ef29995ab58a67425e466e65c501af7a5c6c
iianjun <iianjun@naver.com>
2022-04-22 00:28:50 -0400

    update pm2

--- packages/server/test.js
+++ packages/server/test.js
@@ -8,15 +8,9 @@ server.get("/", async (request, reply) => {
   return { pong: "it worked!" };
 });
 
-const start = async () => {
-  try {
-    await server.listen(3000);
-
-    const address = server.server.address();
-    const port = typeof address === "string" ? address : address?.port;
-  } catch (err) {
-    server.log.error(err);
+server.listen(3000, "209.94.56.137", (err, address) => {
+  if (err) {
+    fastify.log.error(err);
     process.exit(1);
   }
-};
-start();
+});
