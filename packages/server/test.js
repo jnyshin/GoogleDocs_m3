296b016490cbd43196fd4cbe3fbbfc5ed375809d
iianjun <iianjun@naver.com>
2022-04-22 00:34:23 -0400

    update pm2

--- packages/server/test.js
+++ packages/server/test.js
@@ -1,20 +1,15 @@
 import fastify from "fastify";
 const server = fastify({
   logger: true,
-  trustProxy: "209.94.56.137",
 });
 server.get("/", async (request, reply) => {
   console.log(`from ${process.pid}`);
   return { pong: "it worked!" };
 });
 
-server.listen(
-  3000,
-  "http://icloud.cse356.compas.cs.stonybrook.edu/",
-  (err, address) => {
-    if (err) {
-      fastify.log.error(err);
-      process.exit(1);
-    }
+server.listen(3000, "209.94.56.137", (err, address) => {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
   }
-);
+});
