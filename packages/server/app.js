d4de0fc7e657b017b7d375728a6fea1ae3078534
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2022-04-21 22:29:03 -0400

    Revert "update pm2"
    This reverts commit 8a804b46b11ce14e402dbc34ef8e6262d017cdc0.

--- packages/server/app.js
+++ packages/server/app.js
@@ -9,63 +9,74 @@ import fastifyStatic from "fastify-static";
 import fastifyMultipart from "fastify-multipart";
 import os from "os";
 import cluster from "cluster";
-
+import fastify from "fastify";
 const numCore = os.cpus.length; //finds the number of cores in the machine
 
 const { NODE_ENV, SECRET } = process.env;
-export default async (fastify, opts) => {
-  fastify.register(fastifyCors, {});
+const app = fastify({ logger: true });
+app.register(fastifyCors, {});
 
-  fastify.register(fastifyCookie, {
-    secret: SECRET,
-    parseOptions: {},
-  });
-  fastify.register(fastifySession, {
-    secret: SECRET,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24 * 7,
-      httpOnly: true,
-      secure: false,
-    },
-    saveUninitialized: true,
-    resave: true,
-  });
+app.register(fastifyCookie, {
+  secret:
+    "hjadksegdjrkhjadksegdjrkhjadksegdjrkhjadksegdjrkhjadksegdjrkhjadksegdjrk",
+  parseOptions: {},
+});
+app.register(fastifySession, {
+  secret:
+    "hjadksegdjrkhjadksegdjrkhjadksegdjrkhjadksegdjrkhjadksegdjrkhjadksegdjrk",
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true,
+    secure: false,
+  },
+  saveUninitialized: true,
+  resave: true,
+});
 
-  fastify.register(fastifyStatic, {
-    root: join(__dirname, "dist"),
-  });
+app.register(fastifyStatic, {
+  root: join(__dirname, "dist"),
+});
 
-  fastify.register(fastifyMultipart);
+app.register(fastifyMultipart);
 
-  fastify.register(AutoLoad, {
-    dir: join(__dirname, "routes"),
-    options: Object.assign({}, opts),
-  });
-  fastify.addHook("preHandler", (req, res, next) => {
-    req.log.info(`incoming request from ${req.ip}`);
-    if (!req.url.startsWith("/user")) {
-      if (!req.session.user) {
-        res.header("X-CSE356", "61f9f57373ba724f297db6ba");
-        res.send(ERROR_MESSAGE("Not logged in"));
-      }
+app.register(AutoLoad, {
+  dir: join(__dirname, "routes"),
+});
+app.addHook("preHandler", (req, res, next) => {
+  req.log.info(`incoming request from ${req.ip}`);
+  if (!req.url.startsWith("/user")) {
+    if (!req.session.user) {
+      res.header("X-CSE356", "61f9f57373ba724f297db6ba");
+      res.send(ERROR_MESSAGE("Not logged in"));
     }
-    next();
-  });
-  fastify.register((fastifyInstance, options, done) => {
-    mongoose
-      .connect("mongodb://localhost/docs_clone", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then(() => {
-        console.log("mongoose connected");
-      })
-      .catch((err) => {
-        console.error("failed to connect with MongoDB", err);
-        fastifyInstance.close();
-      })
-      .finally(() => done());
-  });
+  }
+  next();
+});
+app.register((fastifyInstance, options, done) => {
+  mongoose
+    .connect("mongodb://localhost/docs_clone", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("mongoose connected");
+    })
+    .catch((err) => {
+      console.error("failed to connect with MongoDB", err);
+      fastifyInstance.close();
+    })
+    .finally(() => done());
+});
+const start = async () => {
+  try {
+    await app.listen(NODE_ENV === "production" ? 80 : 8000);
+    console.log(NODE_ENV);
+    const address = app.server.address();
+    console.log(address);
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
 };
 
 if (numCore > 1) {
@@ -76,5 +87,9 @@ if (numCore > 1) {
     cluster.on("exit", (worker) => {
       console.log("Worker", worker.id, " has exited");
     });
+  } else {
+    start();
   }
+} else {
+  start();
 }
