16c7c01b25400156957e3a93081c0a9147f64c5e
jnyshin <61779849+jnyshin@users.noreply.github.com>
2022-04-23 15:40:49 -0400

    check whether redis returns int or string

--- packages/server/routes/doc.js
+++ packages/server/routes/doc.js
@@ -155,7 +155,10 @@ export default async (fastify, opts) => {
     try {
       const document = await Docs.findById(docId);
       let checkCurrDoc = await redis.sismember("currDoc", docId);
-      if (version !== document.version || checkCurrDoc == "1") {
+      logging.info(
+        `checkCurrDoc is ${checkCurrDoc} with type ${typeof checkCurrDoc}`
+      );
+      if (version !== document.version || checkCurrDoc === 1) {
         logging.info(
           `Version is not matched. client = ${version}, server=${document.version}. OR This doc is being edited right now`,
           id
