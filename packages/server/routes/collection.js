0ab1e994812999d4f06708abc36adb2a72fe42a5
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2022-04-24 02:35:50 -0400

    shareDB

--- packages/server/routes/collection.js
+++ packages/server/routes/collection.js
@@ -54,9 +54,7 @@ export default async (fastify, opts) => {
       const ret = [];
       query.on("ready", async () => {
         query.results.map(async (doc) => {
-          console.log(doc.id);
           const document = await Docs.findById(doc.id);
-          console.log(document);
           ret.push({ id: document.id, name: document.name });
         });
         logging.info(`sent docs list`);
