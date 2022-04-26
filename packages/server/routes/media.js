0d7e25536de020ec95d6917f39b5fc2affb32cd2
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2022-04-26 15:24:11 -0400

    update share_db query

--- packages/server/routes/media.js
+++ packages/server/routes/media.js
@@ -7,7 +7,7 @@ import path from "path";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads");
+    cb(null, "/uploads");
     // cb(null, "/var/www/html/uploads");
   },
 
