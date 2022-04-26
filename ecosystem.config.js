12820e9caab5adff2ace0cde04a25aa7058ce57c
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2022-04-26 11:41:15 -0400

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,28 @@
 module.exports = {
   apps: [
     {
-      name: "docs",
+      name: "APi Server",
       script: "packages/server/app.js",
       watch: true,
       instance_var: "INSTANCE_ID",
       env: {
         PORT: 8000,
-        NODE_ENV: "development",
+        NODE_ENV: "production",
       },
       exec_mode: "cluster",
       instances: "3",
+    },
+    {
+      name: "OP Server",
+      script: "packages/server/app.js",
+      watch: true,
+      instance_var: "INSTANCE_ID",
+      env: {
+        PORT: 8001,
+        NODE_ENV: "production",
+      },
+      exec_mode: "fork",
+      instances: "1",
     },
   ],
 };
