07482e4e3ffb825f45cc613f877a6c61e2f9f92c
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2022-04-26 15:46:48 -0400

    update share_db query

--- packages/client/vite.config.js
+++ packages/client/vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    // outDir: "/var/www/html",
-    outDir: "../server/dist",
+    outDir: "/var/www/html",
+    // outDir: "../server/dist",
     emptyOutDir: true,
   },
   plugins: [react()],
