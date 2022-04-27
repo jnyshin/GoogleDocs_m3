58d91e3228d0210fb30e8d3d131dec33bb2bb3e7
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2022-04-26 21:01:33 -0400

    Revert "seperate server"
    This reverts commit 2dfbc873dac178e222bff68bc84b719530724310.

--- packages/server/app.js
+++ packages/server/app.js
@@ -19,10 +19,10 @@ const { NODE_ENV } = process.env;
 const fastify = Fastify();
 
 const { PORT } = process.env;
-// const IP = "127.0.0.1";
-const IP = process.env.IP
-  ? "icloud.cse356.compas.cs.stonybrook.edu"
-  : "127.0.0.1";
+const IP = "127.0.0.1";
+// const IP = process.env.IP
+//   ? "icloud.cse356.compas.cs.stonybrook.edu"
+//   : "127.0.0.1";
 const RedisStore = connectRedis(fastifySession);
 const ioredis = new IORedis();
 await ioredis.del("clients");
