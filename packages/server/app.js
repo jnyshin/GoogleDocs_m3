d2301da6b1a7b647fad3361a16a7869343ad4114
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2022-04-27 01:03:44 -0400

    fix image cache

--- packages/server/app.js
+++ packages/server/app.js
@@ -87,6 +87,7 @@ fastify.addHook("onRequest", (req, res, next) => {
     const { mediaID } = req.params;
     ioredis.get(mediaID, (err, data) => {
       if (data) {
+        logging.info("image cache hit");
         return res.sendFile(data);
       } else {
         next();
