8aab55f1070c6a0a26c1303155be111cb8b899de
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2022-04-26 21:21:40 -0400

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,18 +9,7 @@ module.exports = {
         NODE_ENV: "production",
       },
       exec_mode: "cluster",
-      instances: "3",
-    },
-    {
-      name: "OP Server",
-      script: "packages/server/app.js",
-      instance_var: "INSTANCE_ID",
-      env: {
-        PORT: 8001,
-        NODE_ENV: "production",
-      },
-      exec_mode: "fork",
-      instances: "1",
+      instances: "4",
     },
   ],
 };
