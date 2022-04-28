65d052f7e642968bbdb35c3c47eec979f1f5799f
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2022-04-28 16:11:22 -0400

    fix

--- packages/server/routes/test.js
+++ packages/server/routes/test.js
@@ -3,19 +3,16 @@ import { fetchAllDocs } from "../store";
 export default async (fastify, opts) => {
   fastify.get(`/`, async (req, res) => {
     const performanceCheck = performaceSensitiveFunc();
-    return { 'took': `${performanceCheck}ms`}
+    return { took: `${performanceCheck}ms` };
   });
   fastify.post(`/pub`, async (req, res) => {
     return {};
   });
 };
 
-
-
-
-function performaceSensitiveFunc() {
-  const start = performance.now()
-  await fetchAllDocs()
+async function performaceSensitiveFunc() {
+  const start = performance.now();
+  await fetchAllDocs();
   const duration = performance.now() - start;
-  return duration
+  return duration;
 }
