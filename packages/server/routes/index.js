79b28c3c826b4385721700f217a3d10c8aff9b4d
jnyshin <61779849+jnyshin@users.noreply.github.com>
2022-04-27 20:41:39 -0400

    Update index.js

--- packages/server/routes/index.js
+++ packages/server/routes/index.js
@@ -12,6 +12,7 @@ import { fetchAllDocs, fetchUpdateDocs } from "../store.js";
 import logging from "../logging.js";
 import { resourceLimits } from "worker_threads";
 import { text } from "express";
+import { QuillDeltaToHtmlConverter } from "quill-delta-to-html";
 
 const ESclient = new Client({
   cloud: {
@@ -24,7 +25,7 @@ const ESclient = new Client({
 });
 var freshData = [];
 setInterval(async function () {
-  //freshData = await makeData(await fetchAllDocs());
+  freshData = await makeData(await fetchAllDocs());
   console.log(await fetchAllDocs());
 }, 5000);
 
