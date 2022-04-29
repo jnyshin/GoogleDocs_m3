41237e20db79b6249d15c075775eb1b2f1d93b82
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2022-04-29 17:31:00 -0400

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -46,41 +46,9 @@ module.exports = {
       exec_mode: "fork",
       instances: "1",
     },
+
     {
       name: "API Server 5",
-      script: "packages/server/app.js",
-      instance_var: "5",
-      env: {
-        PORT: 8005,
-        NODE_ENV: "production",
-      },
-      exec_mode: "fork",
-      instances: "1",
-    },
-    {
-      name: "API Server 6",
-      script: "packages/server/app.js",
-      instance_var: "6",
-      env: {
-        PORT: 8006,
-        NODE_ENV: "production",
-      },
-      exec_mode: "fork",
-      instances: "1",
-    },
-    {
-      name: "API Server 7",
-      script: "packages/server/app.js",
-      instance_var: "7",
-      env: {
-        PORT: 8007,
-        NODE_ENV: "production",
-      },
-      exec_mode: "fork",
-      instances: "1",
-    },
-    {
-      name: "API Server 8",
       script: "packages/server/app.js",
       instance_var: "8",
       env: {
