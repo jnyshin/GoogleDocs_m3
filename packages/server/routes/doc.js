c75a4bcae66653c4d3b0e72e1034903dbc1a22da
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2022-04-29 13:19:59 -0400

    update

--- packages/server/routes/doc.js
+++ packages/server/routes/doc.js
@@ -163,11 +163,9 @@ export default async (fastify, opts) => {
         clients.forEach((client) => {
           if (client.id === id) {
             client.res.write(`data: ${ackStringify(ack)}\n\n`);
-            logging.info("ack sent to ", client.id);
           }
           if (client.docId === docId && client.id !== id) {
             client.res.write(`data: ${opStringify(op)}\n\n`);
-            logging.info("op sent to ", client.id);
           }
         });
 
