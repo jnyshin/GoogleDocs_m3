565816c9785f462938875441e1508e1dd41f5ec4
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2022-05-01 19:46:00 -0400

    test

--- packages/server/app.js
+++ packages/server/app.js
@@ -1,146 +1,143 @@
-import mongoose from "mongoose";
-import ShareDB from "sharedb";
-import MongoShareDB from "sharedb-mongo";
-import {
-  ERROR_MESSAGE,
-  resetIndex,
-  __dirname,
-  elasticStringify,
-  ELASTIC_INDEX,
-} from "./store.js";
-import fastifyCookie from "fastify-cookie";
-import fastifyCors from "fastify-cors";
-import fastifySession from "@fastify/session";
-import fastifyStatic from "fastify-static";
-import fastifyMultipart from "fastify-multipart";
-import fastifyUrlData from "fastify-url-data";
-import IORedis from "ioredis";
-import connectRedis from "connect-redis";
+// import mongoose from "mongoose";
+// import ShareDB from "sharedb";
+// import MongoShareDB from "sharedb-mongo";
+// import {
+//   ERROR_MESSAGE,
+//   resetIndex,
+//   __dirname,
+//   elasticStringify,
+//   ELASTIC_INDEX,
+// } from "./store.js";
+// import fastifyCookie from "fastify-cookie";
+// import fastifyCors from "fastify-cors";
+// import fastifySession from "@fastify/session";
+// import fastifyStatic from "fastify-static";
+// import fastifyMultipart from "fastify-multipart";
+// import fastifyUrlData from "fastify-url-data";
+// import IORedis from "ioredis";
+// import connectRedis from "connect-redis";
 import Fastify from "fastify";
-import logging from "./logging.js";
-import fastifyRedis from "fastify-redis";
-import { join } from "path";
-import richText from "rich-text";
-import Docs from "./schema/docs.js";
-import { v4 as uuid } from "uuid";
-const { NODE_ENV, PORT } = process.env;
+// import logging from "./logging.js";
+// import fastifyRedis from "fastify-redis";
+// import { join } from "path";
+// import richText from "rich-text";
+// import Docs from "./schema/docs.js";
+// import { v4 as uuid } from "uuid";
+// const { NODE_ENV, PORT } = process.env;
 const fastify = Fastify();
-const IP = "127.0.0.1";
-const RedisStore = connectRedis(fastifySession);
-const ioredis = new IORedis();
-import { Client, Serializer } from "@elastic/elasticsearch";
-ShareDB.types.register(richText.type);
-const docsDB = MongoShareDB("mongodb://localhost/docs_clone");
-const backend = new ShareDB({
-  db: docsDB,
-  presence: true,
-  doNotForwardSendPresenceErrorsToClient: true,
-});
+// const IP = "127.0.0.1";
+// const RedisStore = connectRedis(fastifySession);
+// const ioredis = new IORedis();
+// import { Client, Serializer } from "@elastic/elasticsearch";
+// ShareDB.types.register(richText.type);
+// const docsDB = MongoShareDB("mongodb://10.9.4.238:27017/docs_clone");
+// const backend = new ShareDB({
+//   db: docsDB,
+//   presence: true,
+//   doNotForwardSendPresenceErrorsToClient: true,
+// });
 
-export const connection = backend.connect();
+// export const connection = backend.connect();
 
-class MySerializer extends Serializer {
-  serialize(obj) {
-    return elasticStringify(obj);
-  }
-}
-const clientOptions =
-  process.env.NODE_ENV === "production"
-    ? {
-        node: "http://localhost:9200",
-        // Serializer: MySerializer,
-      }
-    : {
-        cloud: {
-          id: "My_deployment:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvJDNkOWI2NmViOTVkMTQ3MmI5YmFhYjQ4NGFhNDhkMmZjJDcwZDk2ZGFiMTJjYjQyZmFiOGJiMTU2NmJkMWM1MGQw",
-        },
-        auth: {
-          username: "elastic",
-          password: "gzq9AcKIBr3BKi7UXuvuutHr",
-        },
-        Serializer: MySerializer,
-      };
+// class MySerializer extends Serializer {
+//   serialize(obj) {
+//     return elasticStringify(obj);
+//   }
+// }
+// const clientOptions =
+//   process.env.NODE_ENV === "production"
+//     ? {
+//         node: "http://localhost:9200",
+//         Serializer: MySerializer,
+//       }
+//     : {
+//         cloud: {
+//           id: "My_deployment:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvJDNkOWI2NmViOTVkMTQ3MmI5YmFhYjQ4NGFhNDhkMmZjJDcwZDk2ZGFiMTJjYjQyZmFiOGJiMTU2NmJkMWM1MGQw",
+//         },
+//         auth: {
+//           username: "elastic",
+//           password: "gzq9AcKIBr3BKi7UXuvuutHr",
+//         },
+//         Serializer: MySerializer,
+//       };
 
-export const ESclient = new Client(clientOptions);
+// export const ESclient = new Client(clientOptions);
 
-fastify.register(fastifyCors, {});
-fastify.register(fastifyCookie, {
-  secret: "2BCC52D156A297EB555F33A2A605E8FB",
-  parseOptions: {},
-});
-fastify.register(fastifySession, {
-  store: new RedisStore({
-    client: ioredis,
-    ttl: 86400,
-  }),
-  secret: "2BCC52D156A297EB555F33A2A605E8FB",
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 7,
-    httpOnly: true,
-    secure: false,
-  },
-  saveUninitialized: true,
-  resave: true,
-});
-fastify.register(fastifyStatic, {
-  root: process.env.NODE_ENV === "production" ? "/" : join(__dirname, "dist"),
-});
+// fastify.register(fastifyCors, {});
+// fastify.register(fastifyCookie, {
+//   secret: "2BCC52D156A297EB555F33A2A605E8FB",
+//   parseOptions: {},
+// });
+// fastify.register(fastifySession, {
+//   store: new RedisStore({
+//     client: ioredis,
+//     ttl: 86400,
+//   }),
+//   secret: "2BCC52D156A297EB555F33A2A605E8FB",
+//   cookie: {
+//     maxAge: 1000 * 60 * 60 * 24 * 7,
+//     httpOnly: true,
+//     secure: false,
+//   },
+//   saveUninitialized: true,
+//   resave: true,
+// });
+// fastify.register(fastifyStatic, {
+//   root: process.env.NODE_ENV === "production" ? "/" : join(__dirname, "dist"),
+// });
 
-fastify.register(fastifyMultipart);
-fastify.register(fastifyRedis, {
-  host: "127.0.0.1",
-  port: 6379, // Redis port
-  family: 4, // 4 (IPv4) or 6 (IPv6)
-});
-fastify.register(fastifyUrlData);
-fastify.addHook("preHandler", (req, res, next) => {
-  logging.info(`incoming request from [${req.url}]`);
-  if (
-    req.url.startsWith("/doc") ||
-    req.url.startsWith("/collection") ||
-    req.url.startsWith("/home") ||
-    req.url.startsWith("/media")
-  ) {
-    if (!req.session.user) {
-      res.header("X-CSE356", "61f9f57373ba724f297db6ba");
-      res.send(ERROR_MESSAGE("Not logged in"));
-    }
-  }
-  if (NODE_ENV === "development" && req.url === "/") {
-    res.redirect(303, "/users/login");
-  }
-  next();
-});
+// fastify.register(fastifyMultipart);
+// fastify.register(fastifyRedis, {
+//   host: "127.0.0.1",
+//   port: 6379, // Redis port
+//   family: 4, // 4 (IPv4) or 6 (IPv6)
+// });
+// fastify.register(fastifyUrlData);
+// fastify.addHook("preHandler", (req, res, next) => {
+//   logging.info(`incoming request from [${req.url}]`);
+//   if (
+//     req.url.startsWith("/doc") ||
+//     req.url.startsWith("/collection") ||
+//     req.url.startsWith("/home") ||
+//     req.url.startsWith("/media")
+//   ) {
+//     if (!req.session.user) {
+//       res.header("X-CSE356", "61f9f57373ba724f297db6ba");
+//       res.send(ERROR_MESSAGE("Not logged in"));
+//     }
+//   }
+//   if (NODE_ENV === "development" && req.url === "/") {
+//     res.redirect(303, "/users/login");
+//   }
+//   next();
+// });
 
-fastify.register(import("./routes/users.js"), {
-  prefix: "/users",
-});
-fastify.register(import("./routes/collection.js"), {
-  prefix: "/collection",
-});
-fastify.register(import("./routes/doc.js"), {
-  prefix: "/doc",
-});
-fastify.register(import("./routes/home.js"), {
-  prefix: "/home",
-});
-fastify.register(import("./routes/media.js"), {
-  prefix: "/media",
-});
-fastify.register(import("./routes/test.js"), {
-  prefix: "/test",
-});
-fastify.register(import("./routes/index.js"), {
-  prefix: "/index",
-});
+// fastify.register(import("./routes/users.js"), {
+//   prefix: "/users",
+// });
+// fastify.register(import("./routes/collection.js"), {
+//   prefix: "/collection",
+// });
+// fastify.register(import("./routes/doc.js"), {
+//   prefix: "/doc",
+// });
+// fastify.register(import("./routes/home.js"), {
+//   prefix: "/home",
+// });
+// fastify.register(import("./routes/media.js"), {
+//   prefix: "/media",
+// });
+// fastify.register(import("./routes/test.js"), {
+//   prefix: "/test",
+// });
 
-fastify.post("/deleteAll", async () => {
-  deleteAll();
-});
+// fastify.post("/deleteAll", async () => {
+//   deleteAll();
+// });
 
 fastify.register((fastifyInstance, options, done) => {
   mongoose
-    .connect("mongodb://localhost/docs_clone", {
+    .connect("mongodb://10.9.4.238:27017/docs_clone", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
