3fa47c3700de6a5d04eed0c38e2735b0f5dd6c9b
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2022-05-01 21:15:50 -0400

    test

--- packages/server/app.js
+++ packages/server/app.js
@@ -48,7 +48,7 @@ class MySerializer extends Serializer {
 const clientOptions =
   process.env.NODE_ENV === "production"
     ? {
-        node: "http://194.113.75.5:9200",
+        node: "http://194.113.75.5",
         Serializer: MySerializer,
       }
     : {
