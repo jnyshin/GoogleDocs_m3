575777970906ac34c907b644fbed698b36d50298
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2022-05-01 21:54:20 -0400

    test

--- packages/server/routes/test.js
+++ packages/server/routes/test.js
@@ -19,7 +19,7 @@ export default async (fastify, opts) => {
           match_all: {},
         },
       });
-      console.log(result.hits.hits[0]._source);
+      console.log(result);
     } catch (err) {
       logging.error(err);
     }
@@ -28,34 +28,17 @@ export default async (fastify, opts) => {
   });
 
   fastify.get(`/index`, async (req, res) => {
-    const query = connection.createFetchQuery(
-      SHARE_DB_NAME,
-      {},
-      {},
-      async (err, results) => {
-        const ret = [];
-        query.results.map((doc) => {
-          const ops = doc.data.ops;
-          const body = new QuillDeltaToHtmlConverter(ops, {})
-            .convert()
-            .replaceAll(/<[\w]*>/gi, "")
-            .replaceAll(/<\/[\w]*>/gi, "")
-            .replaceAll(/<[\w]*\/>/gi, "");
-          ret.push({ docid: doc.id, suggest_body: body, search_body: body });
-        });
-        const operations = ret.flatMap((doc) => [
-          { update: { _id: doc.docid, _index: ELASTIC_INDEX } },
-          {
-            doc: doc,
-          },
-        ]);
-        console.log(operations);
-        await ESclient.bulk({
-          index: ELASTIC_INDEX,
-          operations,
-        });
-      }
-    );
+    ESclient.index({
+      index: ELASTIC_INDEX,
+      id: "1",
+      document: {
+        docid: "1",
+        suggest_name: "hasung",
+        search_name: "hasung",
+        suggest_body: "",
+        search_body: "",
+      },
+    });
 
     return {};
   });
