b54316548aca2c0f3cf985d9a6c3c25f2403aa0d
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2022-05-04 19:48:28 -0400

    update pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       env: {
         PORT: 8000,
         WS_PORT: 8080,
-        WS_IP: "10.9.4.238",
+        WS_IP: "10.9.8.168",
         MONGO_IP: "mongodb://10.9.4.238:27017/docs_clone",
         NODE_ENV: "production",
       },
@@ -23,7 +23,7 @@ module.exports = {
       env: {
         PORT: 8001,
         WS_PORT: 8081,
-        WS_IP: "10.9.4.238",
+        WS_IP: "10.9.8.168",
         MONGO_IP: "mongodb://10.9.4.238:27017/docs_clone",
         NODE_ENV: "production",
       },
@@ -40,7 +40,7 @@ module.exports = {
       env: {
         PORT: 8002,
         WS_PORT: 8082,
-        WS_IP: "10.9.4.238",
+        WS_IP: "10.9.8.168",
         MONGO_IP: "mongodb://10.9.4.238:27017/docs_clone",
         NODE_ENV: "production",
       },
@@ -57,7 +57,7 @@ module.exports = {
       env: {
         PORT: 8003,
         WS_PORT: 8083,
-        WS_IP: "10.9.4.238",
+        WS_IP: "10.9.8.168",
         MONGO_IP: "mongodb://10.9.4.238:27017/docs_clone",
         NODE_ENV: "production",
       },
@@ -72,7 +72,7 @@ module.exports = {
       instance_var: "5",
       env: {
         PORT: 8005,
-        WS_PORT: 8080,
+        WS_PORT: 8084,
         WS_IP: "10.9.8.168",
         MONGO_IP: "mongodb://10.9.4.238:27017/docs_clone",
         NODE_ENV: "production",
@@ -88,7 +88,7 @@ module.exports = {
       instance_var: "6",
       env: {
         PORT: 8006,
-        WS_PORT: 8081,
+        WS_PORT: 8085,
         WS_IP: "10.9.8.168",
         MONGO_IP: "mongodb://10.9.4.238:27017/docs_clone",
         NODE_ENV: "production",
@@ -104,7 +104,7 @@ module.exports = {
       instance_var: "7",
       env: {
         PORT: 8007,
-        WS_PORT: 8082,
+        WS_PORT: 8086,
         WS_IP: "10.9.8.168",
         MONGO_IP: "mongodb://10.9.4.238:27017/docs_clone",
         NODE_ENV: "production",
@@ -120,7 +120,7 @@ module.exports = {
       instance_var: "8",
       env: {
         PORT: 8008,
-        WS_PORT: 8083,
+        WS_PORT: 8087,
         WS_IP: "10.9.8.168",
         MONGO_IP: "mongodb://10.9.4.238:27017/docs_clone",
         NODE_ENV: "production",
@@ -130,61 +130,5 @@ module.exports = {
       out_file: "/dev/null",
       error_file: "/dev/null",
     },
-    // {
-    //   name: "API Server 9",
-    //   script: "packages/server/app.js",
-    //   instance_var: "9",
-    //   env: {
-    //     PORT: 8009,
-    //     WS_PORT: 8080,
-    //     NODE_ENV: "production",
-    //   },
-    //   exec_mode: "fork",
-    //   instances: "1",
-    //   out_file: "/dev/null",
-    //   error_file: "/dev/null",
-    // },
-    // {
-    //   name: "API Server 10",
-    //   script: "packages/server/app.js",
-    //   instance_var: "10",
-    //   env: {
-    //     PORT: 8010,
-    //     WS_PORT: 8081,
-    //     NODE_ENV: "production",
-    //   },
-    //   exec_mode: "fork",
-    //   instances: "1",
-    //   out_file: "/dev/null",
-    //   error_file: "/dev/null",
-    // },
-    // {
-    //   name: "API Server 11",
-    //   script: "packages/server/app.js",
-    //   instance_var: "11",
-    //   env: {
-    //     PORT: 8011,
-    //     WS_PORT: 8082,
-    //     NODE_ENV: "production",
-    //   },
-    //   exec_mode: "fork",
-    //   instances: "1",
-    //   out_file: "/dev/null",
-    //   error_file: "/dev/null",
-    // },
-    // {
-    //   name: "API Server 12",
-    //   script: "packages/server/app.js",
-    //   instance_var: "12",
-    //   env: {
-    //     PORT: 8012,
-    //     WS_PORT: 8083,
-    //     NODE_ENV: "production",
-    //   },
-    //   exec_mode: "fork",
-    //   instances: "1",
-    //   out_file: "/dev/null",
-    //   error_file: "/dev/null",
-    // },
   ],
 };
